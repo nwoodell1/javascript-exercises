c3b00725cb2f38789d0f05fd73a8c7d799ce24cd
nwoodell1 <nathan.woodell@gmail.com>
2024-09-17 18:49:12 -0500

    have cleared the first test in excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = function repeatyboy(word, number) {
 
-};
+    for (let i = 0; i < number; i++) {
+        word.repeat(number)
+
+        return word.repeat(number)
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
