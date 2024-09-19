480ac10e4c3a2ac47a18b8d65f7130b656d3ef14
nwoodell1 <nathan.woodell@gmail.com>
2024-09-18 20:13:48 -0500

    added some notes planning what to do for next time

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function reversethatbitch("string") {
+    //split the string into constituent letters 
+    //store those letters in an array
+    //reproduce the letters in the array backwards
 
+    //we're going to create a for loop that returns the last letter in each iteration of the array
+
+    // 1)create the array with let myArray = string.split("");
+    // 2)access the last letter in the array with myArray.length -1
+    // 3) make sure that we are removing the last letter of the array each time through the loop
 };
 
 // Do not edit below this line
